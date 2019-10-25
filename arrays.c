8081ccff7b5557c55851751c269ed5f9ea932b41
Ziad <ziad.salem@student.manchester.ac.uk>
2019-10-25 13:24:38 +0100

    ex3

--- arrays.c
+++ arrays.c
@@ -3,21 +3,36 @@
 #include "stdlib.h"
 
 /* declare your struct for a person here */
+// struct person which consists of character pointer name
+//and integer age in years
 typedef struct person {
   char* name;
   int age;
 } person;
 
+// insert which takes 4 arguments
+// array of person pointers
+// pointer name
+// age in years
+// next free place in array
 static void insert(person **p, char *name, int age, int *nextfreeplace)
 {
+  // person pointer to an array which is memory allocation of size person
   person *ptr = (person *)malloc(sizeof(person));
-
-  //static int nextfreeplace = 0;
+  // check that malloc allocated memory for array
+  // if not exit
+  if (p == NULL)
+  {
+    exit(0);
+  }
   /* put name and age into the next free place in the array parameter here */
+  // set pointer in the first next free place
   p[*nextfreeplace] = ptr;
+  // put name and age in the next free place in the array parameter
   p[*nextfreeplace]->name = name;
   p[*nextfreeplace]->age = age;
   /* modify nextfreeplace here */
+  // increase next free place by one to point to next position
   (*nextfreeplace)++;
 }
 
@@ -25,19 +40,24 @@ int main(int argc, char **argv)
 {
 
   /* declare the people array here */
+  // person array of size 7
   person *people[HOW_MANY];
+  // next free place in array
   int nextfreeplace = 0;
+  // for loop which calls insert and gives it 4 arguments
+  // people array, next name, next age and the next free place in array
   int i;
   for (i = 0; i <= HOW_MANY; i++)
   {
     insert (people, names[i], ages[i], &nextfreeplace);
   }
+  // for loop to print the contents of people array, name and age
   int z;
   for (z = 0; z <= HOW_MANY; z++)
   {
     printf("Contents for the people's array: %s %d", people[z]->name, people[z]->age);
   }
-
+  // release the memory allocated by malloc
   int x;
   for (x = 0; x <= HOW_MANY; x++)
   {
--- fixed.c
+++ fixed.c
@@ -17,38 +17,64 @@ typedef struct pair {
 // made uppercase
 void process(pair* work)
 {
+  // pointer used by allocating memory for it
+  // of length work.first + 1 * character to have place for every character
   char* temp = (char *)malloc(sizeof(char) * (strlen(work->first) + 1));
+  // for loop for iterating over every character in work.first
   int i;
   for(i=0;i<=strlen(work->first);i++){
+    // current character is recorded here
     char c = work->first[i];
+    // check if upper, save in temp as a small character
     if(isupper(c)){
       temp[i] = tolower(c);
     }
+    // else if small or space save character in temp at this position
     else
       temp[i] = c;
   }
+  // memory allocation for work.second
+  // of length work.first + 1 * character to have place for every character
   work->second = (char *)malloc(sizeof(char) * (strlen(work->first) + 1));
+  // copy contents of temp into work->second
   strcpy(work->second,temp);
+  // variable used to check if character before was a space or not
+  // if yes it is one so change characer at pointer to uppercase else zero.
+  // start with one as first characer is h and we wand to change it to H
   int last_space = 1;
+  // character pointer pointing to start of work->second
   char* ptr = work->second;
-  while(*ptr){
+  // while loop to check if pointer is at the end of work->second
+  while(*ptr != '\0')
+  {
+    // if last_space == 1 change to upper
     if(last_space){
       *ptr = toupper(*ptr);
     }
+    // else set last_space by checking if last character waa a space
     last_space = *ptr == ((char) ' ');
+    // increase pointer by one to point to next character
     ptr++;
   }
+  // deallocate memory used by temp using free
+  free(temp);
 }
 
 // Takes a single string and writes the result to stdout
 int main(int argc, char **argv)
 {
+  // work of type pair to have pair of strings
   pair work;
+  // first pair i staken as an argument
   work.first = argv[1];
 
+  // call processes method and provide pointer of work
+  // to use it as a pointer in processes
   process(&work);
 
+  // printing string again after edit
   printf("%s becomes %s\n",work.first,work.second);
+  // deallocate memory used by work.second using free
   free(work.second);
   return 0;
 }
