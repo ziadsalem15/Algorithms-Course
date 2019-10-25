9583478e96c22f522186a840b59772cc266ede45
Ziad <ziad.salem@student.manchester.ac.uk>
2019-10-25 13:48:15 +0100

    ex3

--- arrays.c
+++ arrays.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "arrays.h"
+#include <string.h>
 #include "stdlib.h"
-
+#include "arrays.h"
 /* declare your struct for a person here */
 // struct person which consists of character pointer name
 //and integer age in years
@@ -18,7 +18,7 @@ typedef struct person {
 static void insert(person **p, char *name, int age, int *nextfreeplace)
 {
   // person pointer to an array which is memory allocation of size person
-  person *ptr = (person *)malloc(sizeof(person));
+  person *ptr = malloc(sizeof(person));
   // check that malloc allocated memory for array
   // if not exit
   if (p == NULL)
@@ -26,11 +26,12 @@ static void insert(person **p, char *name, int age, int *nextfreeplace)
     exit(0);
   }
   /* put name and age into the next free place in the array parameter here */
-  // set pointer in the first next free place
-  p[*nextfreeplace] = ptr;
   // put name and age in the next free place in the array parameter
-  p[*nextfreeplace]->name = name;
-  p[*nextfreeplace]->age = age;
+  //p[*nextfreeplace]->name = name;
+  ptr->name = malloc(sizeof(char) * strlen(name) + 1);
+  strcpy(ptr->name, name);
+  ptr->age = age;
+  p[*nextfreeplace] = ptr;
   /* modify nextfreeplace here */
   // increase next free place by one to point to next position
   (*nextfreeplace)++;
@@ -46,20 +47,18 @@ int main(int argc, char **argv)
   int nextfreeplace = 0;
   // for loop which calls insert and gives it 4 arguments
   // people array, next name, next age and the next free place in array
-  int i;
-  for (i = 0; i <= HOW_MANY; i++)
+
+  for (int i = 0; i < HOW_MANY; i++)
   {
     insert (people, names[i], ages[i], &nextfreeplace);
   }
   // for loop to print the contents of people array, name and age
-  int z;
-  for (z = 0; z <= HOW_MANY; z++)
+  for (int z = 0; z < HOW_MANY; z++)
   {
-    printf("Contents for the people's array: %s %d", people[z]->name, people[z]->age);
+    printf("%d : %s is %d\n",z, people[z]->name, people[z]->age);
   }
   // release the memory allocated by malloc
-  int x;
-  for (x = 0; x <= HOW_MANY; x++)
+  for (int x = 0; x < HOW_MANY; x++)
   {
     free(people[x]);
   }
