532ab13a0cd79d4c8788287829b2df7403138ddf
Ziad Salem <ziad.salem@student.manchester.ac.uk>
2020-01-31 19:37:11 +0000

    ex6

--- skiplist.c
+++ skiplist.c
@@ -116,14 +116,14 @@ void insert(struct skiplist* slist, Value_Type value, int priority){
   {
     level++;
   }
-  /*if (level > slist->levels)
+  if (level > slist->levels)
   {
     for (int i = slist->levels; i < level; i++)
     {
       updates[i] = slist->header;
     }
     slist->levels = level;
-  }*/
+  }
   struct node* new_node = make_node(value, priority, level);
   for(int i=0;i<level;i++){
     new_node->next[i] = updates[i]->next[i];
